b5fab9ed6de5b464bf39cf3a99340e04d4b9bd3c
IAmOscar-Liu <karta0989006@gmail.com>
2021-10-14 13:01:38 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bc9f4fddd8b31814b6673d1a583080eb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7d737649-1f7f-4b81-9342-76d1f5277ecd'
+};
